f45ddc409e1ed362ee5577930f2b7496548ea682
Danyer Dominguez <danjer0826@gmail.com>
2024-10-23 20:19:06 -0400

    feat: add handlers

--- src/Application/CommandQuerySetup.cs
+++ src/Application/CommandQuerySetup.cs
@@ -2,19 +2,25 @@ using Microsoft.Extensions.DependencyInjection;
 using Application.Product.Queries;
 using Application.User.Commands;
 using Application.Product.Commands.CreateProduct;
+using Application.Common;
 
 namespace Application;
 
-public static class CommandQuerySetup {
-    public static IServiceCollection AddCommands(this IServiceCollection services) {
+public static class CommandQuerySetup
+{
+    public static IServiceCollection AddCommands(this IServiceCollection services)
+    {
         services.AddScoped<LoginCommand>();
         services.AddScoped<RegisterCommand>();
+        services.AddScoped<CommandHandler>();
         return services;
     }
 
-    public static IServiceCollection AddQueries(this IServiceCollection services) {
+    public static IServiceCollection AddQueries(this IServiceCollection services)
+    {
         services.AddScoped<GetProductQueryHandler>();
         services.AddScoped<CreateProductCommandHandler>();
+        services.AddScoped<QueryHandler>();
         return services;
     }
 }
